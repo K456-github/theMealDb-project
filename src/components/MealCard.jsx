eef23291c87f6ba9f8a2a1d0ed9727d78babe01a
K456-github <www.shinhaehyun75@gmail.com>
2023-03-07 11:05:27 +0630

    first commit

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import {BiSearch} from "react-icons/bi"
 import "./MealCard.css"
 
 const MealCard = ({name,image}) => {
   return (
-    <div>
+    <div className='relative parent'>
       <img className='h-[300px] object-cover rounded-lg' src={image} alt="" />
+      <p className='icon bg-pink-500 flex items-center justify-center h-12 w-12 absolute top-[45] left-[45]'>
+        <BiSearch className='text-3xl text-white'/>
+      </p>
     </div>
   )
 }
